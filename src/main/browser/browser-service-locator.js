3faab1ae3e7a00dc20eb8cf7e892ed81c322cea4
Eric Smith <genericmoniker@users.noreply.github.com>
2011-11-05 23:09:25 +0000

    Reorganized net service.

--- src/main/browser/browser-service-locator.js
+++ src/main/browser/browser-service-locator.js
@@ -9,7 +9,7 @@ function BrowserServiceLocator() {
     this.authService = authServiceCtor({ httpClient: this.httpClient, localStorage: this.localStorage, logger: this.logger });
     this.siteService = siteServiceCtor({ httpClient: this.httpClient, logger: this.logger, prefsService: this.prefsService });
     this.clipService = clipServiceCtor({ httpClient: this.httpClient, logger: this.logger});
-    this.netService = new NetService();
+    this.netService = netServiceCtor({ httpClient: this.httpClient });
     
     // Resolve dependencies
     this.prefsService.localStorage = this.localStorage;
